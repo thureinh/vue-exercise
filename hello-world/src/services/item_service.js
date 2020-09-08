0f8564f924ec65b542997d5fe7a52fd556c51616
thureinh <thureinhtet2@gmail.com>
2020-09-08 23:57:22 +0630

    fully merged

--- hello-world/src/services/item_service.js
+++ hello-world/src/services/item_service.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-// import axios from 'axios'
-
-// const apiClient = axios.create({
-//   baseURL: 'http://localhost/api',
-//   withCredentials: false,
-//   headers: {
-//     Accept: 'application/json',
-//     'Content-type': 'application/json'
-//   }
-// })
-
-// export default{
-//   // index (Exercise)
-//   getItems(){
-//     return apiClient.get('/items')
-//   },
-
-//   // detail (Detail)
-//   getItem(id){
-//     return apiClient.get('/items/'+id)
-//   }
-// }
-=======
 import axios from 'axios'
 
 const apiClient = axios.create({
@@ -44,4 +20,3 @@ export default{
     return apiClient.get('/items/'+id)
   }
 }
->>>>>>> godsave
